b92bae2875f7ae2acf4850c7d4a06d3cde5ec84b
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-18 14:53:39 -0300

    Finished odd or even challenge using useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,14 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFatorial(num) {
+    const n = parseInt(num)
+    if(n < 0) return -1
+    if(n === 0) return 1
+    return calcFatorial(n - 1) * n
+}
+function imparPar(num) {
+    const n = parseInt(num)
+    if(n % 2 === 0) return 1
+    if(n % 2 !== 0) return -1
+    return imparPar(num % 2)
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+    const [numberIp, setNumberIp] = useState(1)
+    const [ip, setIp] = useState(1)
+
+    useEffect(function() {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    useEffect(function() {
+        if(fatorial > 1000000) {
+            document.title = "Eita!!!"
+        }
+    }, [fatorial])
+
+
+    useEffect(function() {
+        setIp(imparPar(numberIp))
+    }, [numberIp])
+
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
+                </div>
+                <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{ip === 1 ? 'Par' : 'Impar'}</span>
+                </div>
+                <input type="number" className="input" value={numberIp} onChange={e => setNumberIp(e.target.value)} />
+            </div>
+
+
+
         </div>
+
     )
 }
 
